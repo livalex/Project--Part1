02f089a612ea82fe6e59faadab275e24c73e648e
livalex <56893291+livalex@users.noreply.github.com>
2019-11-27 02:45:09 +0200

    Create Constants.java

---
+++ constants/Constants.java
@@ -0,0 +1,24 @@
+package constants;
+
+public class Constants {
+    public static final int STARTING_HP = 100;
+    public static final int STARTING_XP = 0;
+    public static final int STARTING_GROUND = 0;
+    public static final int DEFAULT_ABSCISSA = 0;
+    public static final int DEFAULT_ORDINATE = 0;
+    public static final int DEFAULT_LEVEL = 0;
+    public static final int DEFAULT_PYRO_HP = 500;
+    public static final int PYRO_INCREASE = 50;
+    public static final int FIREBLAST_DAMAGE = 350;
+    public static final int IGNITE_BASE_DAMAGE = 150;
+    public static final float VOLCANIC_GRD_BONUS = 0.25f;
+    public static final float FB_IGN_PYRO_MOD = 0.1f;
+    public static final int IGN_LVL_DMG = 20;
+    public static final int IGN_SEC_LVL_DMG = 30;
+    public static final int DEFAULT_ROGUE_HP = 600;
+    public static final int ROGUE_INCREASE = 40;
+    public static final int DEFAULT_WIZARD_HP= 400;
+    public static final int WIZARD_INCREASE = 30;
+    public static final int DEFAULT_KNIGHT_HP = 900;
+    public static final int KNIGHT_INCREASE = 80;
+}
