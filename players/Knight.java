7b7ff153719c7a7b96c1cc1fa01f82388945f936
livalex <56893291+livalex@users.noreply.github.com>
2019-11-29 01:38:30 +0200

    Update Knight.java

--- players/Knight.java
+++ players/Knight.java
@@ -4,6 +4,7 @@ import constants.Constants;
 
 public class Knight extends Human implements Visitable, Visitor {
     Knight(final int abscissa, final int ordinate) {
+        setPlayerType(1);
         setHp(Constants.DEFAULT_KNIGHT_HP);
         setMaxHp(Constants.DEFAULT_KNIGHT_HP);
         setCurrentAbscissa(abscissa);
@@ -19,23 +20,31 @@ public class Knight extends Human implements Visitable, Visitor {
         }
     }
 
+    @Override
     public void accept(Visitor visitor) {
         visitor.fight(this);
     }
 
     // Visit from visitor
+    @Override
     public void fight(Pyromancer pyromancer) {
-
+        super.checkOverTimeAbility();
+        super.pyroGame(pyromancer, Constants.FB_IGN_KNIGHT_MOD,
+                Constants.FB_IGN_KNIGHT_MOD, Constants.VOLCANIC_GRD_BONUS);
     }
 
+    @Override
     public void fight(Rogue rogue) {
 
     }
 
+    @Override
     public void fight(Knight knight) {
-
+        super.checkOverTimeAbility();
+        super.knightGame(knight, Constants.EXEC_KNIGHT_MOD, Constants.SLAM_KNIGHT_MOD, Constants.LAND_GRD_BONUS);
     }
 
+    @Override
     public void fight(Wizard wizard) {
 
     }
