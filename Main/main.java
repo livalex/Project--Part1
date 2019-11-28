6686b2102dacced1c24c753f05ebd3bfcb03115a
livalex <56893291+livalex@users.noreply.github.com>
2019-11-29 01:36:19 +0200

    Update main.java

--- Main/main.java
+++ Main/main.java
@@ -10,41 +10,16 @@ public class Main {
         InputLoader inputLoader = new InputLoader(args[0], args[1]);
         Input input = inputLoader.load();
         MapBuilder mapBuilder = MapBuilder.getInstance(input.getBattleGround());
+        VectorCreator vectorCreator = VectorCreator.getInstance();
+        ActionCreator actionCreator = ActionCreator.getInstance();
+//        OutputDisplayer outputDisplayer = OutputDisplayer.getInstance();
         PlayersFactory playersFactory = new PlayersFactory();
 
         ArrayList<String> ground = mapBuilder.getBattleGround();
+        ArrayList<Human> players = vectorCreator.createVector(input.getP(), playersFactory, input);
 
-        ArrayList<Human> players = new ArrayList<>();
-
-        for (int i = 0; i < input.getP(); ++i) {
-            String strategy = input.getPlayerTypes().get(i);
-            int ab = input.getFirstCoordonates().get(i);
-            int or = input.getSecondCoordonates().get(i);
-
-            players.add(playersFactory.getPlayer(strategy, ab, or));
-        }
-
-//        System.out.println(input.getN() + " " + input.getM());
-//
-//        for (int i = 0; i < input.getN(); ++i) {
-//            System.out.println(input.getBattleGround().get(i));
-//        }
-//
-//        System.out.println(input.getP());
-//
-//        for (int i = 0; i < input.getP(); ++i) {
-//            System.out.print(input.getPlayerTypes().get(i));
-//            System.out.print(" ");
-//            System.out.print(input.getFirstCoordonates().get(i));
-//            System.out.print(" ");
-//            System.out.print(input.getSecondCoordonates().get(i));
-//            System.out.println();
-//        }
-//
-//        System.out.println(input.getR());
-//
-//        for (int i = 0; i < input.getR(); ++i) {
-//            System.out.println(input.getMoves().get(i));
-//        }
+        players = actionCreator.createMoves(input.getR(), input.getP(), input, players, ground);
+//        outputDisplayer.displayOutput(players);
+        inputLoader.exposeOutput(players);
     }
 }
