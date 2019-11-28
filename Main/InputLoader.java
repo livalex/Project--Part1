bac7ca371bcefba925b86503807e403420c84c9b
livalex <56893291+livalex@users.noreply.github.com>
2019-11-29 01:35:52 +0200

    Update InputLoader.java

--- Main/InputLoader.java
+++ Main/InputLoader.java
@@ -1,6 +1,8 @@
 package main;
 
 import fileio.FileSystem;
+import players.Human;
+
 import java.util.ArrayList;
 
 public final class InputLoader {
@@ -51,5 +53,66 @@ public final class InputLoader {
 
         return new Input(n, m, battleGround, p, playerTypes,
                 firstCoordonates, secondCoordonates, r, moves);
+    }
+
+//    public void exposeDeath(Human player) {
+//        FileSystem fileSystem = new FileSystem(inpPath, outPath);
+//        try {
+//            if (player.getPlayerType() == 0) {
+//                fileSystem.writeCharacter('P');
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    public void exposeOutput(ArrayList<Human> players) {
+        try {
+            FileSystem fileSystem = new FileSystem(inpPath, outPath);
+
+            int j, size = players.size();
+            for (j = 0; j < size; ++j) {
+                Human player = players.get(j);
+                if (player.getHp() <= 0) {
+                    if (player.getPlayerType() == 0) {
+                        fileSystem.writeCharacter('P');
+                    } else if (player.getPlayerType() == 1) {
+                        fileSystem.writeCharacter('K');
+                    } else if (player.getPlayerType() == 2) {
+                        fileSystem.writeCharacter('W');
+                    } else if (player.getPlayerType() == 3) {
+                        fileSystem.writeCharacter('R');
+                    }
+                    fileSystem.writeCharacter(' ');
+                    fileSystem.writeWord("dead");
+                    fileSystem.writeNewLine();
+                } else {
+                    if (player.getPlayerType() == 0) {
+                         fileSystem.writeCharacter('P');
+                    } else if (player.getPlayerType() == 1) {
+                        fileSystem.writeCharacter('K');
+                    } else if (player.getPlayerType() == 2) {
+                        fileSystem.writeCharacter('W');
+                    } else if (player.getPlayerType() == 3) {
+                        fileSystem.writeCharacter('R');
+                    }
+                    fileSystem.writeCharacter(' ');
+                    fileSystem.writeInt(player.getLevel());
+                    fileSystem.writeCharacter(' ');
+                    fileSystem.writeInt(player.getXp());
+                    fileSystem.writeCharacter(' ');
+                    fileSystem.writeInt(player.getHp());
+                    fileSystem.writeCharacter(' ');
+                    fileSystem.writeInt(player.getCurrentAbscissa());
+                    fileSystem.writeCharacter(' ');
+                    fileSystem.writeInt(player.getCurrentOrdinate());
+                    fileSystem.writeNewLine();
+                }
+            }
+
+            fileSystem.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
